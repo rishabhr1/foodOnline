dfa2faad4d279649cf9db550ae695ff648eddf3d
Rishabh Raj <rishabhraj.clg@gmail.com>
2023-08-17 21:10:14 +0530

    cart functionality

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -98,6 +98,13 @@ $(document).ready(function(){
                 {
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     $('#qty-'+food_id).html(response.qty);
+
+                    // subtotal, tax and grand total
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax'],
+                        response.cart_amount['grand_total']
+                    )
                 }
             }
         })
@@ -117,6 +124,7 @@ $(document).ready(function(){
         
         food_id=$(this).attr('data-id');
         url=$(this).attr('data-url');
+        cart_id=$(this).attr('id');
         
         $.ajax({
             type: 'GET',
@@ -136,8 +144,84 @@ $(document).ready(function(){
                 {
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     $('#qty-'+food_id).html(response.qty);
+
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax'],
+                        response.cart_amount['grand_total']
+                    )
+
+                    if(window.location.pathname=='/cart/'){
+                        removeCartItem(response.qty, cart_id);
+                        checkEmptyCart();
+                    }
                 }
             }
         })
     })
+    //delete cart item
+    $('.delete_cart').on('click', function(e){
+        e.preventDefault();
+        
+        cart_id=$(this).attr('data-id');
+        url=$(this).attr('data-url');
+        
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function(response){
+                if(response.status=='Failed')
+                {
+                    swal(response.message, '', 'error')
+                }
+                else
+                {
+                    $('#cart_counter').html(response.cart_counter['cart_count']);
+                    swal(response.status, response.message, "success")
+
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax'],
+                        response.cart_amount['grand_total']
+                    )
+
+                    removeCartItem(0, cart_id);
+                    checkEmptyCart();
+                }
+            }
+        })
+    })
+
+
+    //delete cart element if the qty is 0
+    function removeCartItem(cartItemQty, cart_id){
+        
+            if(cartItemQty<=0)
+            {
+                //remove the cart item element
+                document.getElementById("cart-item-"+cart_id).remove()
+            }
+        
+    }
+
+    //check if the cart is empty
+    function checkEmptyCart(){
+        var cart_counter=document.getElementById('cart_counter').innerHTML
+        if(cart_counter==0){
+            if(cart_counter==0){
+                document.getElementById("empty-cart").style.display="block";
+            }
+        }
+    }
+
+    //apply cart amounts
+    function applyCartAmounts(subtotal, tax, grand_total)
+    {
+        if(window.location.pathname=='/cart/')
+        {
+            $('#subtotal').html(subtotal)
+            $('#tax').html(tax)
+            $('#total').html(grand_total)
+        }
+    }
 });
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
-from .context_processors import get_cart_counter
+from .context_processors import get_cart_counter, get_cart_amounts
 from menu.models import Category, FoodItem
 
 from vendor.models import Vendor
 from .models import Cart
 from django.db.models import Prefetch
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def marketplace(request):
@@ -51,10 +52,10 @@ def add_to_cart(request, food_id):
                     # Increase cart quantity
                     chkCart.quantity+=1
                     chkCart.save()
-                    return JsonResponse({'status': 'Success', 'message': 'Increased Cart quantity', 'cart_counter':get_cart_counter(request), 'qty': chkCart.quantity})
+                    return JsonResponse({'status': 'Success', 'message': 'Increased Cart quantity', 'cart_counter':get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount':get_cart_amounts(request)})
                 except:
                     chkCart=Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
-                    return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart', 'cart_counter':get_cart_counter(request), 'qty': chkCart.quantity})
+                    return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart', 'cart_counter':get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount':get_cart_amounts(request)})
             except:
                 return JsonResponse({'status': 'Failed', 'message': 'This food does not exist'})
         else:
@@ -78,7 +79,7 @@ def decrease_cart(request, food_id):
                     else:
                         chkCart.delete()
                         chkCart.quantity=0
-                    return JsonResponse({'status': 'Success', 'cart_counter':get_cart_counter(request), 'qty': chkCart.quantity})
+                    return JsonResponse({'status': 'Success', 'cart_counter':get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount':get_cart_amounts(request)})
                 except:
                     return JsonResponse({'status': 'Failed', 'message': 'You do not have this item in your cart'})
             except:
@@ -88,9 +89,9 @@ def decrease_cart(request, food_id):
     else:
         return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
 
-
+@login_required(login_url='login')
 def cart(request):
-    cart_items=Cart.objects.filter(user=request.user)
+    cart_items=Cart.objects.filter(user=request.user).order_by('created_at')
     context={
         'cart_items':cart_items,
     }
@@ -104,7 +105,7 @@ def delete_cart(request, cart_id):
                 cart_item=Cart.objects.get(user=request.user, id=cart_id)
                 if cart_item:
                     cart_item.delete()
-                    return JsonResponse({'status': 'Success', 'message': 'Cart item has been deleted!', 'cart_counter':get_cart_counter(request)})
+                    return JsonResponse({'status': 'Success', 'message': 'Cart item has been deleted!', 'cart_counter':get_cart_counter(request), 'cart_amount':get_cart_amounts(request)})
             except:
                 return JsonResponse({'status': 'Failed', 'message': 'Cart Item does not exist'})
         else:
